7cedf7fe68f3dc94147339e5daa09580b996cee5
gulshan <mgulshan2003@gmail.com>
2025-01-13 18:11:41 +0530

    +options in welcomeScreen(), +screenCleaner(), +demo insertion().

--- SlinkedList.c
+++ SlinkedList.c
@@ -10,11 +10,58 @@ struct Node {
 };
 
 void welcomeScreen();
+void screenCleaner();
+
+void insertion();
 
 void main() {
     welcomeScreen();
 }
 
 void welcomeScreen() {
-    printf("\n-- adt-SlinkedList-program.");
+    int choice;
+
+label1:
+    screenCleaner();
+
+    printf("-- Demonstrating linkedList as abstract data type.\n\n");
+
+    printf("Read the options carefully and choose.\n");
+    printf("> 1. Insertion.\n");
+    printf("> 2. Deletion.\n");
+    printf("> 3. Update.\n");
+    printf("> 4. Search.\n");
+    printf("> 5. Sort.\n");
+    printf("> 6. Reverse.\n");
+    printf("> 7. Remove duplicates.\n");
+    printf("> 8. Exit the program.\n\n");
+
+    printf("*NOTE: Before choosing options like deletion, searching,\n"
+           "reverse, search.. must insert enough elements.\n\n");
+
+    printf("Enter your choice: ");
+    scanf("%d", &choice);
+
+    switch(choice) {
+        case 1:
+            insertion();
+            break;
+        case 8:
+            exit(0);
+            break;
+        default:
+            printf("Error! choose from the given options.\n");
+            printf("Press any key to exit...");
+            getch();
+            goto label1;
+    }
+}
+
+void insertion() {
+    printf("\n\n-- Working fine.");
+}
+
+void screenCleaner() {
+    system("cls");
+    fflush(stdin);
 }
